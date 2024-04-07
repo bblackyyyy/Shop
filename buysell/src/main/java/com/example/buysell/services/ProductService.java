0502138c9f79c274e5a7a3b39a417c15e9fc7f04
bblackyyyy <dragochannel.vlad333.vlad@gmail.com>
2024-04-08 01:38:34 +0200

    added database connection

--- buysell/src/main/java/com/example/buysell/services/ProductService.java
+++ buysell/src/main/java/com/example/buysell/services/ProductService.java
@@ -1,13 +1,19 @@
 package com.example.buysell.services;
 
 import com.example.buysell.models.Product;
+import com.example.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
+    private final ProductRepository productRepository;
     private List<Product> products = new ArrayList<>();
     private long ID = 0;
 
@@ -16,22 +22,34 @@ public class ProductService {
         products.add(new Product(++ID, "Iphone 8", "Simple description", 24000, "Ternopil", "Burlia"));
     }
 
-    public List<Product> listProducts() { return products; }
+    public List<Product> listProducts(String title) {
+        List<Product> products = productRepository.findAll();
+        if(title != null){
+           return products = productRepository.findByTitle(title);
+        }
+        else{
+            products = productRepository.findAll();
+        }
+        return products; }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        //product.setId(++ID);
+        log.info("Saving product{}",product);
+        productRepository.save(product);
+        //products.add(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        //products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+//        for (Product product : products) {
+//            if (product.getId().equals(id)) return product;
+//        }
+//        return null;
+        return productRepository.findById(id).orElse(null);
     }
 
 
